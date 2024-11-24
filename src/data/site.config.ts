68d5239d1418765e10333138dac4e83fb3f064c4
Conite002 <conite.gbodogbe@imsp-uac.org>
2024-11-24 09:43:53 +0100

    update config file

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Conité GBODOGBE', // Site author
+	title: 'Exploring Innovations and Challenges in Artificial Intelligence and Data Science', // Site title.
+	description: " Hi, I’m a passionate student specializing in Data Science and Artificial Intelligence.With a strong academic foundation and hands-on experience, I enjoy exploring and discussing the latest innovations in AI, machine learning, and data-driven technologies. This blog is my space to share insights, experiments, and learnings from topics like natural language processing, neural networks, ethical AI, and much more.  Whether it’s breaking down complex concepts, discussing practical applications, or exploring the challenges of AI, I aim to make these ideas accessible and engaging for everyone curious about the world of data science. Join me as we uncover the exciting possibilities of AI and its impact on the future!", // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
